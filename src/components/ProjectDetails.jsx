66d0acc5781141dff8502d4be3688e62a63ac635
Arpenaboyina Rakesh <rakesharpenaboyina2004@gmail.com>
2025-01-12 20:32:38 +0530

    Updated feature X and fixed bug Y

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -5,13 +5,13 @@ const projects = [
     {  
         title: 'Security Checker - Student Security App',  
         description: 'Developed a tracking system for student outings with automated notifications using Node.js and MySQL. Integrated email services for sending alerts to users with pending reports.',  
-        technologies: ['Node.js', 'MySQL', 'Notifications'],  
+        technologies: ['Java Script','Css','Node.js', 'MySQL', 'Notifications'],  
         link: 'https://github.com/Arpenaboyina/Security_Checker_project',  
     },
     {  
         title: 'Ensurance - Insurance Management Platform',  
         description: 'Built a platform for insurance claims and tracking, using Node.js, Express, and MySQL for secure data handling and report generation. Designed RESTful APIs for seamless integration with the frontend.',  
-        technologies: ['Node.js', 'MySQL', 'REST APIs'],  
+        technologies: ['javascript','Css','Node.js', 'MySQL', 'REST APIs'],  
         link: 'https://github.com/Arpenaboyina/ensurance',  
     }  ,
     {  
@@ -25,6 +25,12 @@ const projects = [
         description: 'A fun multiplayer game with real-time updates and winner announcement animations.',  
         technologies: ['React.js', 'CSS', 'JavaScript'],  
         link: 'https://github.com/Arpenaboyina/TIC-TAC-TOE',  
+    }
+    ,{
+        title: 'Travel Services',
+        description:'Book cars effortlessly with our user-friendly app. Enter your name, email, and phone number for personalized service and updates.',
+        technologies:['React js','css','javascript','bootstrap','Node js','Express','Mysql'],
+        link:'https://github.com/Arpenaboyina/Travel_Services'
     }
 ];  
 
